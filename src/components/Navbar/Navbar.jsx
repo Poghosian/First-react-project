05b625d2be2f53971f541d23b90b786453170da2
levon <poghosian93@gmail.com>
2023-01-18 00:07:20 +0400

    changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <a href='/profile'>Profile</a>
+                <a href='/profile'>Profileee</a>
              </div>
             <div className={`${s.item} ${s.active}`}>
                 <a href='/dialogs '>Messages</a>
