b0e6742da718f8334eed64e9acc33bd2ff803f68
Arsen <goginyanarsen@gmail.com>
2023-01-18 18:39:45 +0400

    20, 21, 22

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,50 @@ import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return(
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    asdasdas
+                </div>
+                <div className={s.dialog}>
+                    asdasdas
+                </div>
+                <div className={s.dialog}>
+                    asdasdas
+                </div>
+                <div className={s.dialog}>
+                    asdasdas
+                </div>
+                <div className={s.dialog}>
+                    asdasdas
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    <div className={s.messageImg}>
+                        asdasd
+                    </div>
+                    <div className={s.messageText}>
+                        afafasf
+                    </div>
+                </div>
+                <div className={s.message}>
+                    <div className={s.messageImg}>
+                        afasfaf
+                    </div>
+                    <div className={s.messageText}>
+                            asdasdasd
+                    </div>
+                </div>
+                <div className={s.message}>
+                    <div className={s.messageImg}>
+                            asdasdasdasd
+                    </div>
+                    <div className={s.messageText}>
+                        asdasdasd
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
